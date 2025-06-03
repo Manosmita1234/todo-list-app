ad76826a5567dc1aef43eaf95541629a19e71823
Manosmita Sahu <manosmita.sahu.05@gmail.com>
2025-06-03 23:23:42 +0530

    added code of filtering completed tass

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,4 +1,5 @@
 import { elements } from "./dom.js";
+import { allTasksArray } from "./tasks.js";
 
 export function filterTask(){
     let existing = document.querySelector(".filterTaskDiv");
@@ -9,6 +10,25 @@ export function filterTask(){
     const filterTaskDiv = document.createElement("div");
     filterTaskDiv.className = "filterTaskDiv";
 
+    let pendingTasks = document.createElement("button");
+    pendingTasks.textContent = "Pending";
+
+    let completedtasks = document.createElement("button");
+    completedtasks.textContent = "Completed";
+
     elements.filterTaskButton.appendChild(filterTaskDiv);
+    filterTaskDiv.appendChild(pendingTasks);
+    filterTaskDiv.appendChild(completedtasks);
+
+    completedtasks.addEventListener("click",filterCompletedTasks)
 
 }
+
+function filterCompletedTasks(){
+  
+    allTasksArray.forEach(tasks=>{
+       let completedtasks = tasks.checkbox.checked;
+       tasks.taskContainer.style.display = completedtasks ? "flex":"none";
+    })
+}
+
